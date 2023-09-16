d08a691185d6f98a9a034d85352d03e6caead3f9
jonatasfernandessilva7 <jonatasfernandes@alu.ufc.br>
2023-09-16 15:50:25 -0300

    add testes

--- teste_de_software/test_de_login.py
+++ teste_de_software/test_de_login.py
@@ -40,3 +40,21 @@ class TestesDeLogin():
     self.driver.find_element(By.ID, "id_password").click()
     self.driver.find_element(By.ID, "id_password").send_keys("jonatas")
     self.driver.find_element(By.CSS_SELECTOR, ".btn").click()
+  
+  def test_testeLoginCorreto(self):
+    self.driver.get("http://localhost:8000/login")
+    self.driver.set_window_size(1849, 1080)
+    self.driver.find_element(By.ID, "id_username").click()
+    self.driver.find_element(By.ID, "id_username").send_keys("jonatasteste")
+    self.driver.find_element(By.ID, "id_password").click()
+    self.driver.find_element(By.ID, "id_password").send_keys("jonatas")
+    self.driver.find_element(By.CSS_SELECTOR, ".btn").click()
+
+  def test_testeLoginComUsuarioInexistente(self):
+    self.driver.get("http://localhost:8000/login")
+    self.driver.set_window_size(1849, 1080)
+    self.driver.find_element(By.ID, "id_username").click()
+    self.driver.find_element(By.ID, "id_username").send_keys("joojo")
+    self.driver.find_element(By.ID, "id_password").click()
+    self.driver.find_element(By.ID, "id_password").send_keys("jonatas")
+    self.driver.find_element(By.CSS_SELECTOR, ".btn").click()
